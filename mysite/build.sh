28aa31ae9a1a0a07dc8c5e8843fb4bb6fcbddb56
Saahi30 <saahi.dubey77@gmail.com>
2025-12-06 02:51:26 +0530

    Add debug info to build.sh

--- mysite/build.sh
+++ mysite/build.sh
@@ -2,6 +2,11 @@
 # exit on error
 set -o errexit
 
+echo "Current Directory: $(pwd)"
+ls -la
+echo "Content of requirements.txt:"
+cat requirements.txt
+
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
